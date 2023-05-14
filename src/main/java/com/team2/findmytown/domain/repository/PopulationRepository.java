d0683389851d01e14334267d72f39f05cdff8cdd
nayeon <20192279@sungshin.ac.kr>
2023-05-14 20:27:28 +0900

    [feat] Query to record ranking

--- src/main/java/com/team2/findmytown/domain/repository/PopulationRepository.java
+++ src/main/java/com/team2/findmytown/domain/repository/PopulationRepository.java
@@ -12,4 +12,31 @@ public interface PopulationRepository extends JpaRepository<PopulationEntity, Lo
 
     @Query(value = "SELECT p FROM PopulationEntity  p ORDER BY p.density DESC ")
     List<PopulationEntity> findDensitysOrderedByDescending();
+
+    @Query(value = "SELECT p FROM PopulationEntity  p ORDER BY p.children DESC ")
+    List<PopulationEntity> findChildrensOrderedByDescending();
+
+    @Query(value = "SELECT p FROM PopulationEntity  p ORDER BY p.teen DESC ")
+    List<PopulationEntity> findTeensOrderedByDescending();
+
+    @Query(value = "SELECT p FROM PopulationEntity  p ORDER BY p.twenty DESC ")
+    List<PopulationEntity> findTwentysOrderedByDescending();
+
+    @Query(value = "SELECT p FROM PopulationEntity  p ORDER BY p.thirty DESC ")
+    List<PopulationEntity> findThirtysOrderedByDescending();
+
+    @Query(value = "SELECT p FROM PopulationEntity  p ORDER BY p.fourty DESC ")
+    List<PopulationEntity> findFourtysOrderedByDescending();
+
+    @Query(value = "SELECT p FROM PopulationEntity  p ORDER BY p.fifSix DESC ")
+    List<PopulationEntity> findFifSixsOrderedByDescending();
+
+    @Query(value = "SELECT p FROM PopulationEntity  p ORDER BY p.elder DESC ")
+    List<PopulationEntity> findElderOrderedByDescending();
+
+    @Query(value = "SELECT p FROM PopulationEntity  p ORDER BY p.forign DESC ")
+    List<PopulationEntity> findForignsOrderedByDescending();
+
+
+
 }
