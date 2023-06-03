e232f698159eec66a10d8d9772f8de278ff30ed0
kmc012 <20200979@sungshin.ac.kr>
2023-06-04 01:45:27 +0900

    [Edit] GetMapping Change PostMapping

--- src/main/java/com/team2/findmytown/controller/MainMapController.java
+++ src/main/java/com/team2/findmytown/controller/MainMapController.java
@@ -5,10 +5,7 @@ import com.team2.findmytown.service.SurveyServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -23,17 +20,17 @@ public class MainMapController {
     @Autowired
     private DataServiceImpl dataService;
 
-    @GetMapping("/totalReview")
+    @PostMapping("/totalReview")
     public ResponseEntity<?> importTotalReview (@RequestBody Map<String, String> districtName){
         return ResponseEntity.ok(surveyService.getReviewList(districtName.get("district")));
     }
 
-    @GetMapping("/districtKeyword")
+    @PostMapping("/districtKeyword")
     public ResponseEntity<?> importDistrictKeyword (@RequestBody Map<String, String> distrctName){
         return ResponseEntity.ok(surveyService.getDistrictKeyword(distrctName.get("district")));
     }
 
-    @GetMapping("/realEstateByDistrict")
+    @PostMapping("/realEstateByDistrict")
     public ResponseEntity<?> importRealEstate (@RequestBody Map<String, String> districtName){
         return ResponseEntity.ok(dataService.getRealEstateList(districtName.get("district")));
     }
